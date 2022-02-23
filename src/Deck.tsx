a55cd98ae60e590dea3b1620d5e4c5733e26f405
Richard Huynh <huynhtastic@gmail.com>
2022-02-22 22:46:55 -0600

    feat: swipe removes cards

--- src/Deck.tsx
+++ src/Deck.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useRef, useState } from "react";
+import React, { useCallback, useMemo, useRef, useState } from "react";
 import { Animated, Dimensions, View, PanResponder } from "react-native";
 import { CardData } from "./types";
 
@@ -8,8 +8,8 @@ const SWIPE_THRESHOLD = 0.25 * SCREEN_WIDTH;
 interface Props {
   renderItem: (item: CardData) => React.ReactElement;
   data: CardData[];
-  onSwipeRight: (item: CardData) => void;
-  onSwipeLeft: (item: CardData) => void;
+  onSwipeRight?: (item: CardData) => void;
+  onSwipeLeft?: (item: CardData) => void;
 }
 
 export const Deck: React.FC<Props> = ({
@@ -17,49 +17,31 @@ export const Deck: React.FC<Props> = ({
   data,
   onSwipeLeft = () => {},
   onSwipeRight = () => {},
-  ...props
 }) => {
   const [index, setIndex] = useState(0);
   const position = useRef(new Animated.ValueXY()).current;
-
-  const panResponder = useRef(
-    PanResponder.create({
-      onStartShouldSetPanResponder: () => {
-        return true;
-      },
-      onPanResponderMove: (event, gesture) => {
-        position.setValue({ x: gesture.dx, y: gesture.dy });
-      },
-      onPanResponderRelease: (event, gesture) => {
-        if (gesture.dx > SWIPE_THRESHOLD) {
-          forceSwipe("right");
-        } else if (gesture.dx < -SWIPE_THRESHOLD) {
-          forceSwipe("left");
-        } else {
-          resetPosition();
-        }
-      },
-    })
-  ).current;
-
-  const forceSwipe = useCallback((direction: "right" | "left") => {
-    const x = direction === "right" ? SCREEN_WIDTH : -SCREEN_WIDTH;
-    Animated.timing(position, {
-      toValue: { x, y: 0 },
-      duration: 250,
-      useNativeDriver: false,
-    }).start(() => onSwipeComplete(direction));
-  }, []);
 
   const onSwipeComplete = useCallback(
     (direction: "right" | "left") => {
       const item = data[index];
 
       direction === "right" ? onSwipeRight(item) : onSwipeLeft(item);
-      position.setValue({ x: 0, y: 0 });
       setIndex(index + 1);
+      position.setValue({ x: 0, y: 0 });
     },
-    [index]
+    [data, index, onSwipeLeft, onSwipeRight, position]
+  );
+
+  const forceSwipe = useCallback(
+    (direction: "right" | "left") => {
+      const x = direction === "right" ? SCREEN_WIDTH : -SCREEN_WIDTH;
+      Animated.timing(position, {
+        toValue: { x, y: 0 },
+        duration: 250,
+        useNativeDriver: false,
+      }).start(() => onSwipeComplete(direction));
+    },
+    [onSwipeComplete, position]
   );
 
   const resetPosition = useCallback(() => {
@@ -67,7 +49,29 @@ export const Deck: React.FC<Props> = ({
       toValue: { x: 0, y: 0 },
       useNativeDriver: false,
     }).start();
-  }, []);
+  }, [position]);
+
+  const panResponder = useMemo(
+    () =>
+      PanResponder.create({
+        onStartShouldSetPanResponder: () => {
+          return true;
+        },
+        onPanResponderMove: (event, gesture) => {
+          position.setValue({ x: gesture.dx, y: gesture.dy });
+        },
+        onPanResponderRelease: (event, gesture) => {
+          if (gesture.dx > SWIPE_THRESHOLD) {
+            forceSwipe("right");
+          } else if (gesture.dx < -SWIPE_THRESHOLD) {
+            forceSwipe("left");
+          } else {
+            resetPosition();
+          }
+        },
+      }),
+    [forceSwipe, position, resetPosition]
+  );
 
   const getCardStyle = useCallback(() => {
     const rotate = position.x.interpolate({
@@ -79,8 +83,10 @@ export const Deck: React.FC<Props> = ({
   }, [position]);
 
   const renderCards = useCallback(() => {
-    return data.map((item, index) => {
-      if (index === 0) {
+    return data.map((item, i) => {
+      if (i < index) {
+        return null;
+      } else if (i === index) {
         return (
           <Animated.View
             key={item.id}
@@ -94,7 +100,7 @@ export const Deck: React.FC<Props> = ({
 
       return renderItem(item);
     });
-  }, [renderItem, data]);
+  }, [renderItem, data, index, getCardStyle, panResponder.panHandlers]);
 
   return <View>{renderCards()}</View>;
 };
