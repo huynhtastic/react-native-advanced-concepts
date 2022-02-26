b45c79de1497c0740b98ca109d1d312d7a482889
Richard Huynh <huynhtastic@gmail.com>
2022-02-26 16:22:09 -0600

    feat: add creatUser feature

--- one-time-password/functions/src/createUser.ts
+++ one-time-password/functions/src/createUser.ts
@@ -1,5 +1,20 @@
 import { https } from "firebase-functions";
+import { auth } from "firebase-admin";
 
 export const createUser: Parameters<typeof https.onRequest>[0] = (req, res) => {
-  res.send(req.body);
+  // Verify provided phone
+  if (!req.body.phone) {
+    return res.status(422).send({ error: "Bad Input" });
+  }
+
+  // Sanitize dashes and parents
+  const phone = String(req.body.phone).replace(/[^\d]/g, "");
+
+  // Create user w/ phone
+  auth()
+    .createUser({ uid: phone })
+    .then((user) => res.send(user))
+    .catch((err) => res.status(422).send({ error: err }));
+
+  // Respond with account made
 };
--- one-time-password/functions/src/index.ts
+++ one-time-password/functions/src/index.ts
@@ -1,16 +1,11 @@
+import admin from "firebase-admin";
 import * as functions from "firebase-functions";
 import { createUser as _createUser } from "./createUser";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", { structuredData: true });
-  response.send("Hello from Firebase!");
-});
+import serviceAccount from "../service_account.json";
 
-export const goodBye = functions.https.onRequest((req, res) => {
-  res.send("Goodbye!");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
 });
 
 export const createUser = functions.https.onRequest(_createUser);
