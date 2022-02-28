63c434cac0a172e41dd2f95d2c413e39f8c9f6eb
Richard Huynh <huynhtastic@gmail.com>
2022-02-27 22:35:32 -0600

    feat: handle button submit

--- firebase-auth/components/SignUpForm.tsx
+++ firebase-auth/components/SignUpForm.tsx
@@ -1,6 +1,15 @@
 import React, { useState } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { StyleSheet } from "react-native";
 import { Input, Button } from "react-native-elements";
+import axios from "axios";
+
+const BASE_URL =
+  "https://us-central1-one-time-password-1083b.cloudfunctions.net";
+
+const handleSubmit = async (phone: string) => {
+  await axios.post("createUser", { phone }, { baseURL: BASE_URL });
+  await axios.post("requestOtp", { phone }, { baseURL: BASE_URL });
+};
 
 export const SignUpForm: React.FC = () => {
   const [phone, setPhone] = useState("");
