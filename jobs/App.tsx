c409d3ab809bfee31c203b0a0e7f550ed59ca9ac
Richard Huynh <huynhtastic@gmail.com>
2022-03-06 16:12:44 -0600

    add bottom tab

--- jobs/App.tsx
+++ jobs/App.tsx
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+import { AuthScreen } from "./screens/AuthScreen";
+import { WelcomeScreen } from "./screens/WelcomeScreen";
+
+const Tab = createBottomTabNavigator<{ Auth: undefined; Welcome: undefined }>();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // <View style={styles.container}>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Welcome" component={WelcomeScreen} />
+        <Tab.Screen name="Auth" component={AuthScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+    // </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
