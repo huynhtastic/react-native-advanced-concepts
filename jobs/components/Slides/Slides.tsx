083eb2b859a8a8b6be270c5b2aaf8945b5a55f3e
Richard Huynh <huynhtastic@gmail.com>
2022-03-06 21:26:52 -0600

    show background colors for each page

--- jobs/components/Slides/Slides.tsx
+++ jobs/components/Slides/Slides.tsx
@@ -1,15 +1,19 @@
 import React, { useCallback } from "react";
-import { ScrollView, Text, View, StyleSheet } from "react-native";
+import { Dimensions, ScrollView, Text, View, StyleSheet } from "react-native";
 
+const SCREEN_WIDTH = Dimensions.get("window").width;
 interface Props {
-  data: { text: string }[];
+  data: { text: string; color: string }[];
 }
 
 export const Slides: React.FC<Props> = ({ data }) => {
   const renderSlides = useCallback(() => {
     return data.map((slide) => {
       return (
-        <View style={styles.slide} key={slide.text}>
+        <View
+          style={[styles.slide, { backgroundColor: slide.color }]}
+          key={slide.text}
+        >
           <Text style={styles.slideText}>{slide.text}</Text>
         </View>
       );
@@ -17,7 +21,7 @@ export const Slides: React.FC<Props> = ({ data }) => {
   }, [data]);
 
   return (
-    <ScrollView horizontal style={{ minHeight: "100%" }}>
+    <ScrollView pagingEnabled horizontal style={{ minHeight: "100%" }}>
       {renderSlides()}
     </ScrollView>
   );
@@ -26,10 +30,12 @@ export const Slides: React.FC<Props> = ({ data }) => {
 const styles = StyleSheet.create({
   slide: {
     flex: 1,
+    width: SCREEN_WIDTH,
     justifyContent: "center",
     alignItems: "center",
   },
   slideText: {
     fontSize: 30,
+    color: "white",
   },
 });
--- jobs/screens/WelcomeScreen.tsx
+++ jobs/screens/WelcomeScreen.tsx
@@ -3,8 +3,9 @@ import { Text, View } from "react-native";
 import { Slides } from "../components/Slides/Slides";
 
 const SLIDE_DATA = [
-  { text: "Welcome to job app" },
-  { text: "Set your location, then swipe away" },
+  { text: "Welcome to job app", color: "#03A9F4" },
+  { text: "Use this to get a job", color: "#009688" },
+  { text: "Set your location, then swipe away", color: "#03A9F4" },
 ];
 
 export const WelcomeScreen: React.FC = () => {
