a7169e7ac4be76e4b0128479b9110ffc16d2eb78
Richard Huynh <huynhtastic@gmail.com>
2022-03-06 21:35:14 -0600

    navigate from welcome to auth

--- jobs/components/Slides/Slides.tsx
+++ jobs/components/Slides/Slides.tsx
@@ -1,5 +1,7 @@
+import { useNavigation } from "@react-navigation/native";
 import React, { useCallback } from "react";
 import { Dimensions, ScrollView, Text, View, StyleSheet } from "react-native";
+import { Button } from "react-native-elements";
 
 const SCREEN_WIDTH = Dimensions.get("window").width;
 interface Props {
@@ -7,18 +9,36 @@ interface Props {
 }
 
 export const Slides: React.FC<Props> = ({ data }) => {
+  const { navigate } = useNavigation();
+
+  const renderLastSlide = useCallback(
+    (index: number) => {
+      if (index === data.length - 1) {
+        return (
+          <Button
+            onPress={() => navigate("Auth")}
+            buttonStyle={styles.buttonStyle}
+            title="Onwards!"
+          />
+        );
+      }
+    },
+    [data]
+  );
+
   const renderSlides = useCallback(() => {
-    return data.map((slide) => {
+    return data.map((slide, index) => {
       return (
         <View
           style={[styles.slide, { backgroundColor: slide.color }]}
           key={slide.text}
         >
           <Text style={styles.slideText}>{slide.text}</Text>
+          {renderLastSlide(index)}
         </View>
       );
     });
-  }, [data]);
+  }, [data, renderLastSlide]);
 
   return (
     <ScrollView pagingEnabled horizontal style={{ minHeight: "100%" }}>
@@ -33,9 +53,14 @@ const styles = StyleSheet.create({
     width: SCREEN_WIDTH,
     justifyContent: "center",
     alignItems: "center",
+    textAlign: "center",
   },
   slideText: {
+    textAlign: "center",
     fontSize: 30,
     color: "white",
+  },
+  buttonStyle: {
+    marginTop: 15,
   },
 });
