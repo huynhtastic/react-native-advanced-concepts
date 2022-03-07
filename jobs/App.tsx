6c5f412cc8de68e91b4d2afaffa3e9d0667aa32b
Richard Huynh <huynhtastic@gmail.com>
2022-03-06 20:50:04 -0600

    add all navigators

--- jobs/App.tsx
+++ jobs/App.tsx
@@ -1,31 +1,68 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Text } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import { AuthScreen } from "./screens/AuthScreen";
 import { WelcomeScreen } from "./screens/WelcomeScreen";
+import { MapScreen } from "./screens/MapScreen";
+import { DeckScreen } from "./screens/DeckScreen";
+import { ReviewScreen } from "./screens/ReviewScreen";
+import { SettingsScreen } from "./screens/SettingsScreen";
 
-const Tab = createBottomTabNavigator<{ Auth: undefined; Welcome: undefined }>();
+const Tab = createBottomTabNavigator<{
+  Auth: undefined;
+  Welcome: undefined;
+  Main: undefined;
+}>();
+
+const MainTab = createBottomTabNavigator<{
+  Map: undefined;
+  Deck: undefined;
+  ReviewStack: undefined;
+}>();
+
+const Stack = createNativeStackNavigator<{
+  Review: undefined;
+  Settings: undefined;
+}>();
+
+const StackNavigator = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Review"
+        component={ReviewScreen}
+        options={{
+          title: "Review Jobs",
+          headerRight: () => {
+            return <Text>Go Right</Text>;
+          },
+        }}
+      />
+      <Stack.Screen name="Settings" component={SettingsScreen} />
+    </Stack.Navigator>
+  );
+};
+
+const MainTabNavigator = () => {
+  return (
+    <MainTab.Navigator>
+      <MainTab.Screen name="Map" component={MapScreen} />
+      <MainTab.Screen name="Deck" component={DeckScreen} />
+      <MainTab.Screen name="ReviewStack" component={StackNavigator} />
+    </MainTab.Navigator>
+  );
+};
 
 export default function App() {
   return (
-    // <View style={styles.container}>
     <NavigationContainer>
       <Tab.Navigator>
         <Tab.Screen name="Welcome" component={WelcomeScreen} />
         <Tab.Screen name="Auth" component={AuthScreen} />
+        <Tab.Screen name="Main" component={MainTabNavigator} />
       </Tab.Navigator>
     </NavigationContainer>
-    // </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
---
+++ jobs/screens/DeckScreen.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export const DeckScreen: React.FC = () => {
+  return (
+    <View>
+      <Text>DeckScreen</Text>
+      <Text>DeckScreen</Text>
+      <Text>DeckScreen</Text>
+      <Text>DeckScreen</Text>
+      <Text>DeckScreen</Text>
+    </View>
+  );
+};
