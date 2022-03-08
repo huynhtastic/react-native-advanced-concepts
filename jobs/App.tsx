0a5dd35b73cc5c66ba4290b85f44bad83d1953ea
Richard Huynh <huynhtastic@gmail.com>
2022-03-07 21:44:49 -0600

    setup redux

--- jobs/App.tsx
+++ jobs/App.tsx
@@ -1,13 +1,17 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { WelcomeTabs } from "./src/navigators/WelcomeTabs";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <SafeAreaView style={{ flex: 1 }}>
-        <WelcomeTabs />
-      </SafeAreaView>
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <SafeAreaView style={{ flex: 1 }}>
+          <WelcomeTabs />
+        </SafeAreaView>
+      </NavigationContainer>
+    </Provider>
   );
 }
