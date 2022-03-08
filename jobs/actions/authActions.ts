f4223ff97cd7468eeebe9fa1a6fca54e5f862b18
Richard Huynh <huynhtastic@gmail.com>
2022-03-07 22:13:58 -0600

    finish login action

--- jobs/actions/authActions.ts
+++ jobs/actions/authActions.ts
@@ -1,20 +1,40 @@
-import { ThunkAction } from "redux-thunk";
+import { ThunkAction, ThunkDispatch } from "redux-thunk";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { FACEBOOK_LOGIN_SUCCESS } from "./types";
+import { FACEBOOK_LOGIN_FAIL, FACEBOOK_LOGIN_SUCCESS } from "./types";
 import { DefaultRootState } from "react-redux";
 import { Action } from "redux";
+import Facebook from "expo-facebook";
 
 export const facebookLogin =
   (): ThunkAction<
     void,
     DefaultRootState,
     void,
-    Action<typeof FACEBOOK_LOGIN_SUCCESS>
+    Action<typeof FACEBOOK_LOGIN_SUCCESS | typeof FACEBOOK_LOGIN_FAIL>
   > =>
   async (dispatch) => {
     const token = await AsyncStorage.getItem("fb_token");
     if (token) {
       dispatch({ type: FACEBOOK_LOGIN_SUCCESS, payload: token });
     } else {
+      doFacebookLogin(dispatch);
     }
   };
+
+const doFacebookLogin = async (
+  dispatch: ThunkDispatch<
+    DefaultRootState,
+    void,
+    Action<"FACEBOOK_LOGIN_SUCCESS" | "FACEBOOK_LOGIN_FAIL">
+  >
+) => {
+  await Facebook.initializeAsync("343123017746716");
+  const result = await Facebook.logInWithReadPermissionsAsync({
+    permissions: ["public_profile"],
+  });
+  if (result.type === "cancel") {
+    return dispatch({ type: FACEBOOK_LOGIN_FAIL });
+  }
+
+  await AsyncStorage.setItem("fb_token", result.token);
+};
