e7f0003791b23d352a91d5976d0bf230ccb4b070
Richard Huynh <huynhtastic@gmail.com>
2022-03-20 17:46:25 -0500

    add renderCard scaffold

--- jobs/src/screens/DeckScreen.tsx
+++ jobs/src/screens/DeckScreen.tsx
@@ -1,16 +1,45 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import { connect, ConnectedProps } from "react-redux";
 import { actions } from "../actions";
 import { MainTabsScreenProps } from "../navigators/MainTabs";
+import MapView from "react-native-maps";
+import { Card, Button, Text } from "react-native-elements";
+import { Swipe } from "../components/Swipe/Swipe";
 
 type PropsFromRedux = ConnectedProps<typeof connector>;
 
 type Props = PropsFromRedux & MainTabsScreenProps<"Deck">;
 
-export const _DeckScreen: React.FC<Props> = () => {
-  return <View></View>;
+export const _DeckScreen: React.FC<Props> = ({ jobs }) => {
+  // @ts-ignore
+  const renderCard = (job) => {
+    return (
+      <Card>
+        <Card.Title>{job.jobtitle}</Card.Title>
+        <View style={styles.detailWrapper}>
+          <Text>{job.company}</Text>
+          <Text>{job.formattedRelativeTime}</Text>
+        </View>
+        <Text>{job.snippet.replace(/<b>/g, "").replace(/<\/b>/g, "")}</Text>
+      </Card>
+    );
+  };
+
+  return (
+    <View>
+      <Swipe data={jobs} renderItem={renderCard} />
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  detailWrapper: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginBottom: 10,
+  },
+});
 
 // @ts-ignore
 const mapStateToProps = ({ jobs }) => {
